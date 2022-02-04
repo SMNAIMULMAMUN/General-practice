5791952e79483b6c982a9ee57a2366bab167f8e0
SMNAIMULMAMUN <smnaimulmamun11@gmail.com>
2022-02-04 19:32:55 +0600

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #finding the number of digits in a large number
+
+# In[ ]:
+
+
+num = input("enter a number :")
+num_digits = 0
+while num_digits < len(num) :
+    num_digits +=1
+print("num of digits is :", num_digits)
+
+
+# In[ ]:
+
+
+
+
